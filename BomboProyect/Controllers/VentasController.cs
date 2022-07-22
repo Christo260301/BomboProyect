d5b0961cf30232271d2708fecf39d29499bde29a
Max <angello.ramirez014@gmail.com>
2022-07-21 21:41:19 -0500

    Vistas para Venta

---
+++ BomboProyect/Controllers/VentasController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BomboProyect.Models;
+
+namespace BomboProyect.Controllers
+{
+    public class VentasController : Controller
+    {
+        private BomboDBContext db = new BomboDBContext();
+
+        // GET: Ventas
+        public ActionResult Index()
+        {
+            return View(db.Ventas.ToList());
+        }
+
+        // GET: Ventas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ventas ventas = db.Ventas.Find(id);
+            if (ventas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ventas);
+        }
+
+        // GET: Ventas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ventas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "VentaId,Fechaventa,HoraVenta,Status")] Ventas ventas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ventas.Add(ventas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ventas);
+        }
+
+        public ActionResult _ListProductos() {
+            return View(db.Productos.ToList());
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
